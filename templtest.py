486f58d57996f879a9a95c33ba819295256f9450
Albert Vonpupp <vonpupp@gmail.com>
2013-04-19 16:57:46 -0300

    refactored some variable names

--- templtest.py
+++ templtest.py
@@ -6,18 +6,21 @@ from string import Template
 #     delimiter = '%'
 #     idpattern = '[a-z]+_[a-z]+'
 
-params = { 'fqdn':'br210008.congregacao.org.br',
-           'nameserver1':'127.0.0.1',
-           'nameserver2':'8.8.8.8',
-           'gw':'8.8.8.8',
-           'rndcfile':'8.8.8.8',
-           'rndckey':'8.8.8.8',
-           'subnet':'8.8.8.8',
-           'mask':'8.8.8.8',
-           'dnsforwardzone':'8.8.8.8',
-           'dnsreversezone':'8.8.8.8',
-           'dhcpdstart':'8.8.8.8',
-           'dhcpdend':'8.8.8.8',
+params = { 'uuid':'br210008',
+#           'domain':'br210008.congregacao.org.br',
+           'ccdomain':'congregacao.org.br',
+           'server':'10.0.0.1',
+#           'nameserver1':'10.0.0.1',
+           'nameserver2':'208.67.220.220',
+#           'gw':'10.0.0.1',
+           'rndcfile':'/etc/named.d/dns.key',
+           'rndckey':'dhcp_updates',
+           'subnet':'10.0.0.0',
+           'netmask':'255.255.252.0',
+           'dhcpdstart':'10.0.0.31',
+           'dhcpdend':'10.0.3.240',
+#           'dnsforwardzone':'br210008.congregacao.org.br.',
+           'dnsreversezone':'0.0.10.in-addr.arpa',
          }
 
 fin = open('conf/dhcpd.conf')
