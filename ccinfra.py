0d2be52ba70d41ef012039f4927bdb60e71757a0
Albert Vonpupp <vonpupp@gmail.com>
2013-04-29 17:09:18 -0300

    Switched to argparse instead of optparse

--- ccinfra.py
+++ ccinfra.py
@@ -38,53 +38,19 @@ __copyright__ = "2013 Congregacao Crista no Brasil / Albert De La Fuente"
 
 import getopt
 import sys
+import argparse
+
 
 def main(argv):
-
-    try:
-        optlist, args = getopt.getopt(argv, 'hv:p:aingo:t', [
-            'help', 'verbose',
-            'parse'
-            'export-all', 'export-rfi', 'export-rfn', 'in-objects',
-            'parse-v7', 'parse-v8', 'run-tests', 'strict-tests'])
-    except getopt.GetoptError, msg:
-        sys.stderr.write("ccinfra: error: %s" % msg)
-        sys.stderr.write("See 'ccinfra --help'.\n")
-        return 1
+    # Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--verbose", help="increase output verbosity",
+                        action="store_true")
+    args = parser.parse_args()
 
     # Create structures
     rb = ReqBox()
     rb.parserverion = 7
-
-    # Parse options
-    for opt, optarg in optlist:
-        if opt in ('-h', '--help'):
-            sys.stdout.write(__doc__)
-            return 0
-        elif opt in ('-v', '--verbose'):
-            log_setverbosity(int(optarg))
-            pass
-        elif opt in ('-p', '--parse'):
-            if optarg.lower() == 'v7':
-                rb.parserverion = 7
-            elif optarg.lower() == 'v8':
-                rb.parserverion = 8
-        elif opt in ('-o', '--in-objects'):
-            rb.inputdir = optarg
-
-        # Map options to structures
-        rb.inputfile = args[0]
-        rb.parseall = rb.parseall or opt in ('-a', '--export-all')
-        rb.parsefun = rb.parseall or rb.parsefun or opt in ('-f', '--export-fun')
-        rb.parserfi = rb.parseall or rb.parserfi or opt in ('-i', '--export-rfi')
-        rb.parserfn = rb.parseall or rb.parserfn or opt in ('-r', '--export-rfn')
-        rb.parsernf = rb.parseall or rb.parsernf or opt in ('-n', '--export-rnf')
-        rb.parsergn = rb.parseall or rb.parsergn or opt in ('-g', '--export-rgn')
-        rb.parseext = rb.parseall or rb.parseext or opt in ('-e', '--export-ext')
-        rb.parseinc = rb.parseall or rb.parseinc or opt in ('-n', '--export-inc')
-        rb.parseimp = rb.parseall or rb.parseimp or opt in ('-m', '--export-imp')
-        rb.runtests = rb.runtests or opt in ('-t', '--run-tests')
-        rb.stricttests = rb.stricttests or opt == '--strict-tests'
 
     # Initialize structures
     rb.initparser()
