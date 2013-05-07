6348adc82405d25ad8f96283e236f13e5bf118de
Albert De La Fuente <vonpupp@gmail.com>
2013-05-07 18:26:25 -0300

    Manually merged model/model.py from bertioga branch

--- ccinfra/model/model.py
+++ ccinfra/model/model.py
@@ -1,81 +1,91 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
+
+import os
+from filebuilder import FileBuilder
+
 
 class GeneralSetup():
     """
     Attributes:
     """
-    
+
     def __init__(self):
         pass
 
     def is_opensuse_host():
-        pass
+        return os.file_exists('/etc/SuSE-release')
 
-    def replace_copy(self, input_file, input_expr, output_file, output_expr):
-        f = open(input_file)
-        contents = f.read()
-        replaced_contents = contents.replace(input_expr, output_expr)
 
 class ServerSetup(GeneralSetup):
     """
     Attributes:
     """
-    root_conf = '.conf/srv/'
-    
+    root_conf_out = '/etc'
+    root_conf_in = 'conf/srv/'
+    fb = None
+
     def __init__(self):
-        pass
-    
+        self.fb = FileBuilder()
+        self.fb.set_in_path(self.root_conf_out)
+        self.fb.set_out_path(self.root_conf_in)
+
     def network_setup(self):
         pass
-    
+
     def packages_setup(self):
         pass
 
     def vpn_setup(self):
         pass
-    
+
     def deploy_setup(self):
         pass
 
     def ntp_setup(self):
         pass
 
-    def dhcp_setup(self):
+    def isc_dhcpd_setup(self):
+        # Explicit paths are better than implicit
+        self.fb.build_file('/etc/dhcpd.conf')
+
+    def named_keys_setup(self):
+        self.fb.build_file('/etc/dhcpd.conf')
+
+    def named_setup(self):
+        self.fb.build_file('named.conf')
+        self.fb.build_file('named.conf.include')
+
+    def openldap_setup(self):
         pass
-    
-    def dns_setup(self):
-        pass
-    
-    def ldap_setup(self):
-        pass
-    
+
     def ccbsist_setup(self):
         pass
-    
+
     def nfs_setup(self):
         pass
-    
+
     def samba_setup(self):
         pass
-    
+
     def firewall_setup(self):
         pass
 
     def setup_all(self):
         pass
 
+
 class ClientSetup(GeneralSetup):
     """
     Attributes:
     """
     root_conf = '.conf/cli/'
-    
+
     def __init__(self):
         pass
 
     def network_setup(self):
         pass
-    
+
     def packages_setup(self):
         pass
 
@@ -84,10 +94,10 @@ class ClientSetup(GeneralSetup):
 
     def ldap_setup(self):
         pass
-    
+
     def autofs_setup(self):
         pass
-    
+
     def firewall_setup(self):
         pass
 
