944d8508678f195f5f3a683bf6e22f0eaa3bde5f
Albert De La Fuente <vonpupp@gmail.com>
2013-05-21 10:16:40 -0300

    Modified test_file_output

--- tests/test_file_output.py
+++ tests/test_file_output.py
@@ -11,16 +11,16 @@ srv.set_out_path('out/')
 srv.isc_dhcpd_setup()
 
 
-assert PIL.Image.VERSION[:3] == '1.1'
-
-# Create an image and do stuff with it.
-assert (im.mode, im.size) == ('1', (100, 100))
-assert len(im.tobytes()) == 1300
-
-# Create images in all remaining major modes.
-im = PIL.Image.new("P", (100, 100))
-im = PIL.Image.new("RGB", (100, 100))
-im = PIL.Image.new("I", (100, 100))
-im = PIL.Image.new("F", (100, 100))
+#assert PIL.Image.VERSION[:3] == '1.1'
+#
+## Create an image and do stuff with it.
+#assert (im.mode, im.size) == ('1', (100, 100))
+#assert len(im.tobytes()) == 1300
+#
+## Create images in all remaining major modes.
+#im = PIL.Image.new("P", (100, 100))
+#im = PIL.Image.new("RGB", (100, 100))
+#im = PIL.Image.new("I", (100, 100))
+#im = PIL.Image.new("F", (100, 100))
 
 print("ok")
