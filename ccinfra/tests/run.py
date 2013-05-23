9cd26f9131eb62224e5d47fd027acf3519144494
Albert De La Fuente <vonpupp@gmail.com>
2013-05-23 18:03:55 -0300

    Cleaned run.py a little bit

--- ccinfra/tests/run.py
+++ ccinfra/tests/run.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
-import random
+from unittest import *
 import sys
-import inspect
-import codecs
-import csv
 from ccinfra.model.filebuilder import FileBuilder
 
 
@@ -17,57 +13,13 @@ class FileBuilderTestCase(unittest.TestCase):
 
     def setUp(self):
         self.fb = FileBuilder()
-        #self.seq = range(10)
+        self.fb.build_file('/etc/dhcpd.conf')
 
-    #def uckeymeasure(self, key):
-    #    #result = self.rb.model.fp.fundict[key][1:] #and key
-    #    result = key #and key
-    #    return result
+    def test_dhcpd_file_exists(self):
+        exists = os.path.exists(LOCAL_INSTALL_DIR)
+        assertTrue(exists)
 
-    def tag_check(self, filename, d, tagstr):
-        """
-        Verifica se foi pulado algum tagstr no dict d e guarda os resultados
-        no arquivo filename
-        """
-        fh = codecs.open(filename, encoding='utf-8', mode='w') # open(filename, 'r')
-        csvhdlr = csv.writer(fh, delimiter='\t')#, quotechar='"')#, quoting=csv.QUOTE_MINIMAL)
-
-        itemscount = len(d)
-        maxid = max(d)
-        if maxid.startswith(tagstr):
-            maxid = maxid[len(tagstr):]
-        #item = max(d, key=self.uckeymeasure)
-        #maxid = max(self.uckeymeasure(k) for k in d.keys())
-        csvhdlr.writerow(["ID", "Tipo", "Pulados"])
-        for number in range(1, int(maxid) + 1):
-            item = tagstr + '%03d' % number
-            reqid = item
-            reqname = ''
-            reqtype = tagstr
-            reqbody = ''
-            try:
-                self.assertIn(item, d)
-                status = 'ok'
-            except:
-                status = 'FAILED'
-                if self.rb.stricttests:
-                    raise
-            row = [reqid, reqtype, status]
-            csvhdlr.writerow(row)
-
-    def test_parser_0101_missing_uc_objects(self):
-        """
-        Verifica se foi pulado algum numero de UC na sequencia
-        """
-        filename = sys._getframe().f_code.co_name + '.csv'
-        d = self.rb.model.fp.fundict
-        tagstr = 'UC'
-        self.tag_check(filename, d, tagstr)
-
-    def test_parser_0102_missing_rfi_objects(self):
-        """
-        Verifica se foi pulado algum numero de RFI na sequencia
-        """
+    def test_dhcpd_file_line_count(self):
         filename = sys._getframe().f_code.co_name + '.csv'
         d = self.rb.model.uniquerfi
         tagstr = 'RFI'
@@ -75,32 +27,14 @@ class FileBuilderTestCase(unittest.TestCase):
 
 
 def fast_tests_suite():
-    #args = sys.argv[1:]
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestMissingObjects))
-    suite.addTest(unittest.makeSuite(TestOrphanObjects))
-    suite.addTest(unittest.makeSuite(TestOrphanUCRel))
-    suite.addTest(unittest.makeSuite(TestFuzzyStrMatch))
+    suite.addTest(unittest.makeSuite(FileBuilderTestCase))
     return suite
 
 def slow_test_suite():
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestEAIntegrity))
+    suite.addTest(unittest.makeSuite(FileBuilderTestCase))
     return suite
 
 if __name__ == '__main__':
-#    unittest.main(verbosity=2)
-
-
-    suiteFew = unittest.TestSuite()
-    #suiteFew.addTest(TestMissingObjects("test_01_UC_missing"))
-    #suiteFew.addTest(TestMissingObjects("test_shuffle"))
-    #unittest.TextTestRunner(verbosity=2).run(suiteFew)
     unittest.TextTestRunner(verbosity=2).run(fast_tests_suite())
-
-#suite1 = unittest.TestSuite(MyTestCase1(), MyTestCase2())
-#suite2 = unittest.TestSuite()
-#suite2.addTest(MyOtherTestCase())
-#
-#big_suite = unittest.TestSuite(suite1)
-#big_suite.addTest(suite2)
