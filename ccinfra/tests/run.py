3a154ce52746f763bb8b0a40967edc3d659d32e4
Albert Vonpupp <vonpupp@gmail.com>
2013-05-27 20:01:24 -0300

    Refactored imports

--- ccinfra/tests/run.py
+++ ccinfra/tests/run.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
+from unittest import TestCase, TestSuite, makeSuite, TextTestRunner, assertTrue
 import sys
 from ccinfra.model.filebuilder import FileBuilder
 import os
 #import pudb
 
+INPATH = '../../conf/srv/'
+OUTPATH = 'out2/'
 
-class FileBuilderTestCase(unittest.TestCase):
+class FileBuilderTestCase(TestCase):
     fb = None
 
     def setUp(self):
         self.fb = FileBuilder()
-        self.fb.set_in_path('../../conf/srv/')
+        self.fb.set_in_path(INPATH)
         #self.fb.set_in_path('../../conf/srv/etc')
-        self.fb.set_out_path('out2/')
+        self.fb.set_out_path(OUTPATH)
         
     def test_000paths(self):
         self.fb.set_file_in('/etc/dhcpd.conf')
@@ -24,25 +26,22 @@ class FileBuilderTestCase(unittest.TestCase):
 #         self.fb.get_io_paths()
 
     def test_dhcpd_file_exists(self):
-        exists = os.path.exists(LOCAL_INSTALL_DIR)
+        exists = os.path.exists(OUTPATH + 'dhcpd.conf')
         assertTrue(exists)
 
     def test_dhcpd_file_line_count(self):
-        filename = sys._getframe().f_code.co_name + '.csv'
-        d = self.rb.model.uniquerfi
-        tagstr = 'RFI'
-        self.tag_check(filename, d, tagstr)
+        pass
 
 
 def fast_tests_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(FileBuilderTestCase))
+    suite = TestSuite()
+    suite.addTest(makeSuite(FileBuilderTestCase))
     return suite
 
 def slow_test_suite():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(FileBuilderTestCase))
+    suite = TestSuite()
+    suite.addTest(makeSuite(FileBuilderTestCase))
     return suite
 
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(fast_tests_suite())
+    TextTestRunner(verbosity=2).run(fast_tests_suite())
